b4905ef81a8bee4e785b180a1f90ad21d9f555a4
kemoelamorim <kemoel.amorim@gmail.com>
2021-06-27 21:03:59 -0300

    aula 01 com exemplos

--- type-boolean.js
+++ type-boolean.js
@@ -7,4 +7,11 @@ console.log(primeiroNumero === segundoNumero)
 const texto1 = "alura"
 const texto2 = "Alura"
 
+// compara letras maiúculas e minúculas 
 console.log(texto1 === texto2)
+
+const bacharel = false
+const tecnico = 0
+
+// compara valores
+console.log(bacharel == tecnico)
