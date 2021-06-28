42115e65a5316b804556ea6a97042bc481ab3775
kemoelamorim <kemoel.amorim@gmail.com>
2021-06-27 21:19:53 -0300

    aula 01 com type-null

---
+++ type-null.js
@@ -0,0 +1,12 @@
+let input = null;
+let input2;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+
+console.log(input); // null
+console.log(input2); // undefined
